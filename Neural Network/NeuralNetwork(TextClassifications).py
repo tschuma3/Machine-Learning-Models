056a3723d1231abe6bf3c929f1a7e6875c7e91dc
Tyler Schumacher <tschuma3@uccs.edu>
2023-01-11 17:44:30 -0700

    Added the ability to read in text files and save the model in a .h5 file

--- Neural Network/NeuralNetwork(TextClassifications).py
+++ Neural Network/NeuralNetwork(TextClassifications).py
@@ -10,7 +10,7 @@ The Goal of the neural network is to determine whether a movie review is good or
 #Load data 
 data = keras.datasets.imdb
 
-(train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=10000) #num_words=10000 is only getting the 10000 most frequent words
+(train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=88000) #num_words=10000 is only getting the 10000 most frequent words
 
 #Creating a mapping
     #Typically will have to do own mapping
@@ -29,17 +29,19 @@ word_index["<UNUSED>"] = 3
 reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
 
 #Redefine training and testing data and trim down/normalize
-train_data = keras.preprocessing.sequence.pad_sequences(train_data, values=word_index["<PAD>"], padding="post", maxlen=250)
-test_data = keras.preprocessing.sequence.pad_sequences(test_data, values=word_index["<PAD>"], padding="post", maxlen=250)
+train_data = keras.preprocessing.sequence.pad_sequences(train_data, value=word_index["<PAD>"], padding="post", maxlen=250)
+test_data = keras.preprocessing.sequence.pad_sequences(test_data, value=word_index["<PAD>"], padding="post", maxlen=250)
 
 #Decoding the training and testing data
 def decode_review(text):
     return " ".join([reverse_word_index.get(i, "?") for i in text]) #The "?" is a defult value so the program does not crash
 
+#Only done when no model is saved
+"""
 #Defining the model
 model = keras.Sequential()
     #Embedding 10000 == the amount of words -- 16 == the dimensions
-model.add(keras.layers.Embedding(10000, 16)) #Group words that are similar ---- math version explination: finds word vectors for each word that is passed in and passes those vectors to the next layer
+model.add(keras.layers.Embedding(88000, 16)) #Group words that are similar ---- math version explination: finds word vectors for each word that is passed in and passes those vectors to the next layer
 model.add(keras.layers.GlobalAveragePooling1D()) #Takes whatever dimension the data is in and puts it to a lower dimension
 model.add(keras.layers.Dense(16, activation="relu")) #Squishes everything between 0 and 1
 model.add(keras.layers.Dense(1, activation="sigmoid")) #Same as the layer above
@@ -51,18 +53,49 @@ model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"]
 
 #Splitting the data
 x_val = train_data[:10000]
-x_train = train_labels[10000:]
+x_train = train_data[10000:]
 
-y_val = train_data[:10000]
+y_val = train_labels[:10000]
 y_train = train_labels[10000:]
 
     #batch_size == the amount of data fed into the model
 fitModel = model.fit(x_train, y_train, epochs=40, batch_size=512, validation_data=(x_val, y_val), verbose=1)
 
 results = model.evaluate(test_data, test_labels)
-
 print(results)
 
+#Saving the model
+model.save("model.h5")
+"""
+
+#Loads the mode
+model = keras.models.load_model("model.h5")
+
+#Use this to open a specific text file named "test.txt" that has a review
+"""
+def review_encode(s):
+    encoded = [0]
+
+    for word in s:
+        if word.lower() in word_index:
+            encoded.append(word_index[word.lower()])
+        else:
+            encoded.append(2)
+
+    return encoded
+
+with open("test.txt", encoding="utf-8") as f:
+	for line in f.readlines():
+		nline = line.replace(",", "").replace(".", "").replace("(", "").replace(")", "").replace(":", "").replace("\"","").strip().split(" ")
+		encode = review_encode(nline)
+		encode = keras.preprocessing.sequence.pad_sequences([encode], value=word_index["<PAD>"], padding="post", maxlen=250) # make the data 250 words long
+		predict = model.predict(encode)
+		print(line)
+		print(encode)
+		print(predict[0])
+"""
+
+"""
 #Predicting
 test_review = test_data[0]
 predict = model.predict([test_review])
@@ -70,3 +103,4 @@ print("Review: ")
 print(f"Prediction: {str(predict[0])}")
 print(f"Actual: {str(test_labels[0])}")
 print(results)
+"""
